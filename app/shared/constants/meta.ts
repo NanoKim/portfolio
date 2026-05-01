0b4c4c0f207891cceb5353956b69191544f5ced1
Jaeyeong Kim <kjyyy7341@gmail.com>
2026-05-01 20:13:51 +0900

    chore : fix ogImage
    chore : fix ogImage

--- app/shared/constants/meta.ts
+++ app/shared/constants/meta.ts
@@ -3,5 +3,5 @@ export const META = {
   description: "비즈니스 요구사항을 명확한 코드로 풀어내는 실전형 개발자 김재영입니다.",
   siteUrl: "https://portfolio-nine-bay-43.vercel.app/",
   siteName: "NanoKim Portfolio",
-  ogImage: "/profile.png",
+  ogImage: "/profile.jpg",
 };
