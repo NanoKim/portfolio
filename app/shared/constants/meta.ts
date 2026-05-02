42599e8e3b26794d42b23c65efec426be2f343a5
Jaeyeong Kim <kjyyy7341@gmail.com>
2026-05-02 23:15:03 +0900

    design : change thumbnail
    design : change thumbnail

--- app/shared/constants/meta.ts
+++ app/shared/constants/meta.ts
@@ -3,5 +3,5 @@ export const META = {
   description: "추상적인 비즈니스 요구사항을 명확하고 견고한 코드로 설계하는 실전형 개발자 김재영입니다.",
   siteUrl: "https://portfolio-nine-bay-43.vercel.app/",
   siteName: "NanoKim Portfolio",
-  ogImage: "/profile.jpg",
+  ogImage: "/thumbnail.png",
 };
